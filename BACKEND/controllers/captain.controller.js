d0f40517d9223212495a13bccd19e82d6c47d837
RohitBedwal <rohitbedwal09@gmail.com>
2025-02-24 16:13:19 +0530

    captain authorized

--- BACKEND/controllers/captain.controller.js
+++ BACKEND/controllers/captain.controller.js
@@ -1,6 +1,8 @@
 const captainModel = require("../models/captain.model");
 const captainServices = require("../services/captain.services");
 const {validationResult} = require('express-validator');
+const cookieParser = require('cookie-parser');
+const blacklistTokenModel = require("../models/blacklistToken.model");
 
 module.exports.registerCaptain = async (req,res,next)=>{
     const errors = validationResult(req);
@@ -31,4 +33,32 @@ module.exports.registerCaptain = async (req,res,next)=>{
 
     res.status(201).json({token,captain})
 
+}
+
+module.exports.loginCaptain = async (req,res,next)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    const {email,password} = req.body;
+    const captain = await captainModel.findOne({email}).select('+password');
+    if(!captain){
+        return res.status(401).json({message:'Invalid Email or Password'})
+    }
+    const isMatch = await captain.comparePassword(password);
+    if(!isMatch){
+        return res.status(401).json({message:'Invalid Email or Password'})
+    }
+    const token = captain.generateAuthToken();
+    res.cookie('token',token)
+    res.status(200).json({token,captain})
+}
+module.exports.getProfile = async (req,res,next)=>{
+    return res.status(200).json(req.captain)
+}
+module.exports.logoutCaptain = async (req,res,next)=>{
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    await blacklistTokenModel.create({token})
+    res.clearCookie('token')
+    return res.status(200).json({message:'logged out'})
 }
--- BACKEND/routes/captain.routes.js
+++ BACKEND/routes/captain.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {body} = require('express-validator');
 const captainController = require('../controllers/captain.controller');
-
+const authMiddleware = require('../middlewares/auth.middleware');
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({min:3}).withMessage('First name should be 3 characters long'),
@@ -14,5 +14,12 @@ router.post('/register',[
 ],
     captainController.registerCaptain
 )
-
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min:6}).withMessage('Password must be at least 6 characters long')
+],
+    captainController.loginCaptain
+)
+router.get('/profile',authMiddleware.authCaptain,captainController.getProfile)
+router.get('/logout',authMiddleware.authCaptain,captainController.logoutCaptain)
 module.exports= router;
